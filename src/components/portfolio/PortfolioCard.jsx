c5c9ac0ca112a354600f41d06613f385bb07a04a
Abolfazl <68917563+Abolfazl26s@users.noreply.github.com>
2025-06-06 20:21:01 +0330

    Update PortfolioCard.jsx

--- src/components/portfolio/PortfolioCard.jsx
+++ src/components/portfolio/PortfolioCard.jsx
@@ -6,12 +6,12 @@ export default function PortfolioCard({ project }) {
   if (project.isDone)
     return (
       <div className="col-xl-4 col-md-6 mb-3 d-flex align-items-center justify-content-center flex-column">
-        <Link href={project.link} target="_blank" rel="noopener noreferrer">
+        <Link to={project.link} target="_blank" rel="noopener noreferrer">
           <ShapeMonitor imgSrc={project.imgSrc} projectName={project.name} />
         </Link>
         <div className="pt-4 pb-3 d-flex flex-column align-items-start justify-content-center">
           <p className="text-wrap fw-bolder">
-            <Link href={project.link} target="_blank" rel="noopener noreferrer">
+            <Link to={project.link} target="_blank" rel="noopener noreferrer">
               {console.log(project.link)}
               <i className="fas fa-link"></i>
               <span className="text-dark ms-1">{project.name}</span>
