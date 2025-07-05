fbbbea8143edb98656c2e13048038653045dff97
frontEndIcc <asaeidabadi243@gmail.com>
2025-07-05 17:02:43 +0330

    added :cursor pointer for monitor and project name in porfilecard

--- src/components/portfolio/PortfolioCard.jsx
+++ src/components/portfolio/PortfolioCard.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ShapeMonitor from "./ShapeMonitor";
-import { Link } from "react-router-dom";
-import CustomLink from "../exLink";
 
 export default function PortfolioCard({ project }) {
   if (project.isDone)
     return (
       <div className="col-xl-4 col-md-6 mb-3 d-flex align-items-center justify-content-center flex-column">
-        <div onClick={() => {
-                window.location.href = `${project.link}`;
-              }}>
+        <div
+          onClick={() => {
+            window.location.href = `${project.link}`;
+          }}
+        >
           <ShapeMonitor imgSrc={project.imgSrc} projectName={project.name} />
         </div>
         <div className="pt-4 pb-3 d-flex flex-column align-items-start justify-content-center">
@@ -18,11 +18,15 @@ export default function PortfolioCard({ project }) {
               <i className="fas fa-link"></i>
               <span className="text-dark ms-1">{project.name}</span>
             </Link> */}
-            <div onClick={() => {
+            <div
+              onClick={() => {
                 window.location.href = `${project.link}`;
-              }}>
+              }}
+            >
               <i className="fas fa-link"></i>
-              <span className="text-dark ms-1">{project.name}</span>
+              <span style={{ cursor: "pointer" }} className="text-dark ms-1">
+                {project.name}
+              </span>
             </div>
           </p>
           <p>
