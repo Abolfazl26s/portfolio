4f5db6e6c4e90bca8a3a809325c4b78a3b5be31c
frontEndIcc <asaeidabadi243@gmail.com>
2025-08-14 20:18:15 +0100

    update

--- src/i18n.js
+++ src/i18n.js
@@ -1,24 +1,54 @@
+// import i18n from "i18next";
+// import { initReactI18next } from "react-i18next";
+// import enTranslation from "./locales/en/translation.json";
+// import faTranslation from "./locales/fa/translation.json";
+
+// const resources = {
+//   en: {
+//     translation: enTranslation,
+//   },
+//   fa: {
+//     translation: faTranslation,
+//   },
+// };
+
+// i18n.use(initReactI18next).init({
+//   resources,
+//   lng: "fa", // زبان پیش‌فرض را تنظیم کنید
+//   fallbackLng: "en",
+//   interpolation: {
+//     escapeValue: false,
+//   },
+// });
+
+// export default i18n;
+
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import enTranslation from "./locales/en/translation.json";
-import faTranslation from "./locales/fa/translation.json";
+import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-const resources = {
-  en: {
-    translation: enTranslation,
-  },
-  fa: {
-    translation: faTranslation,
-  },
-};
+const apiURL = process.env.REACT_APP_API_URL;
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: "fa", // زبان پیش‌فرض را تنظیم کنید
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    lng: "fa",
+    fallbackLng: "en",
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: apiURL,
+    },
+    onLanguageChanged: (lng) => {
+      // این تابع با تغییر زبان اجرا می‌شود
+      // و استایل و جهت صفحه را تغییر می‌دهد
+      document.body.dir = lng === "fa" ? "rtl" : "ltr";
+    },
+  });
 
 export default i18n;
